b33a0789f6ea039899abdb9cfc0e24ea4bf53284
joesiveSkill <joesive@gmail.com>
2025-12-06 16:21:26 +0700

    Simplify dashboard - remove all UI imports

--- src/app/dashboard/admin/courses/new/page.tsx
+++
@@ -1,9 +0,0 @@
-import { CourseForm } from '@/components/course/course-form'
-
-export default function NewCoursePage() {
-  return (
-    <div className="p-6">
-      <CourseForm mode="create" />
-    </div>
-  )
-}
